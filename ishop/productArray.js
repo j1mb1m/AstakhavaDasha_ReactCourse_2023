0e75a732208ef011d19a5287e43e89c1ae56864b
Dasha Astakhava <dasha.kozhalo@gmail.com>
2023-06-18 10:23:55 +0300

    first version task

---
+++ ishop/productArray.js
@@ -0,0 +1,50 @@
+var productArray = [
+    {
+        id: 1,
+        title: 'Игровой ноутбук Machenike Star 15 S15-i512450H30504GF144LHD0BY',
+        price: 3559.00,
+        url: 'https://content2.onliner.by/catalog/device/header/8a8e3b38fe8f3a696be3b51d522011a2.jpeg',
+        description: '15.6" 1920 x 1080, IPS, 144 Гц, Intel Core i5 12450H, 16 ГБ DDR4, SSD 512 ГБ, видеокарта NVIDIA GeForce RTX 3050 4 ГБ, без ОС, цвет крышки черный',
+        availableQuantity: 10
+    },
+    {
+        id: 2,
+        title: 'Игровой ноутбук Lenovo IdeaPad Gaming 3 15ARH7 82SB00KYTX',
+        price: 2699.00,
+        url: 'https://content2.onliner.by/catalog/device/header/4d910be83039a9d61c62c98e95edd1f4.jpeg',
+        description: '15.6" 1920 x 1080, IPS, 120 Гц, AMD Ryzen 5 6600H, 16 ГБ DDR5, SSD 512 ГБ, видеокарта NVIDIA GeForce RTX 3050 4 ГБ, Windows 11, цвет крышки черный',
+        availableQuantity: 7
+    },
+    {
+        id: 3,
+        title: 'Игровой ноутбук Lenovo Legion 5 15ITH6 82JK005BPB',
+        price: 2797.00,
+        url: 'https://content2.onliner.by/catalog/device/header/51e57f978184cfe422cfcc5eec4c49bb.jpeg',
+        description: '15.6" 1920 x 1080, IPS, 165 Гц, Intel Core i5 11400H, 16 ГБ DDR4, SSD 512 ГБ, видеокарта NVIDIA GeForce RTX 3050 4 ГБ, без ОС, цвет крышки темно-синий',
+        availableQuantity: 17
+    },
+    {
+        id: 4,
+        title: 'Игровой ноутбук Lenovo IdeaPad Gaming 3 15IAH7 82S900KHRM',
+        price: 3099.00,
+        url: 'https://content2.onliner.by/catalog/device/header/02d287f50d913db7bf93be65dd4fb6c2.jpeg',
+        description: '15.6" 1920 x 1080, IPS, 120 Гц, Intel Core i5 12450H, 16 ГБ DDR4, SSD 512 ГБ, видеокарта NVIDIA GeForce RTX 3060 6 ГБ, без ОС, цвет крышки темно-серый',
+        availableQuantity: 17
+    },
+    {
+        id: 5,
+        title: 'Игровой ноутбук Machenike L17 Gen 12 L17-i512500H30606GQ165HHD0R2',
+        price: 3256.00,
+        url: 'https://content2.onliner.by/catalog/device/header/84ab92ed753ff3c1438ff31895a0da81.jpeg',
+        description: '17.3" 2560 x 1440, IPS, 165 Гц, Intel Core i5 12500H, 16 ГБ DDR4, SSD 512 ГБ, видеокарта NVIDIA GeForce RTX 3060 6 ГБ, без ОС, цвет крышки черный',
+        availableQuantity: 17
+    },
+    {
+        id: 6,
+        title: 'Игровой ноутбук MSI GF63 Thin 11SC-623XRU',
+        price: 2178.79,
+        url: 'https://content2.onliner.by/catalog/device/header/02d287f50d913db7bf93be65dd4fb6c2.jpeg',
+        description: '15.6" 1920 x 1080, IPS, 60 Гц, Intel Core i5 11400H, 8 ГБ DDR4, SSD 256 ГБ, видеокарта NVIDIA GeForce GTX 1650 4 ГБ GDDR6, без ОС, цвет крышки черный',
+        availableQuantity: 1
+    },
+];
